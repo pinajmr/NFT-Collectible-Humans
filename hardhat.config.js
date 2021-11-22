b1c8e75219493338285acb3383971982bd794a2f
Jose Pina <pinajoma0@gmail.com>
2021-11-22 19:47:20 +0100

    add polygon red

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const RINKEBY_PROJECT_ID = process.env.RINKEBY_PROJECT_ID;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const MUMBAI_PROJECT_ID = process.env.MUMBAI_PROJECT_ID;
+const POLYGON_PROJECT_ID = process.env.POLYGON_PROJECT_ID;
 
 module.exports = {
   solidity: "0.8.4",
@@ -15,6 +16,10 @@ module.exports = {
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${MUMBAI_PROJECT_ID}`,
       accounts:[PRIVATE_KEY]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${POLYGON_PROJECT_ID}`
     }
+
   }
 }
