57d37d3263aa954c59570196339e93ae65719bd8
Jose Pina <pinajoma0@gmail.com>
2021-11-22 18:04:21 +0100

    apply PaymentSplitter

--- contracts/GalleryArt.sol
+++ contracts/GalleryArt.sol
@@ -4,19 +4,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 
-contract GalleryArt is ERC721, ERC721Enumerable {
+contract GalleryArt is ERC721, ERC721Enumerable, PaymentSplitter {
     using Counters for Counters.Counter;
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
 
-    constructor (uint256 _maxSupply) ERC721("GalleryArt","GASP") {
+    constructor(address[] memory _payees, uint256[] memory  _shares, uint256 _maxSupply) ERC721("PinaPunks", "PP") PaymentSplitter(_payees, _shares) payable {
         maxSupply = _maxSupply;
     }
 
     function mint() public {
+        require(msg.value >= 50000000000000000,"you neet 0.05 ETH to mint the ArtGallery");
         uint256 current = _idCounter.current();
         require(current < maxSupply, "No ArtGallery left:(" );
         _safeMint(msg.sender, current);
